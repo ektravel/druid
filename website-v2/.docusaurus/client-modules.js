2c7e29f8c341e12a088d403fdd4155917f890854
Katya Macedo <katya.macedo@imply.io>
2022-10-06 15:55:06 -0500

    Site compiles

---
+++ website-v2/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/katyamacedo/Documents/Repos/druid/website-v2/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/katyamacedo/Documents/Repos/druid/website-v2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/katyamacedo/Documents/Repos/druid/website-v2/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/katyamacedo/Documents/Repos/druid/src/css/customTheme.css'),
+];
